b4e4dc7169aac51c2d8954b5c2b85b906fa4919f
Sondos <sondosaljaru@gmail.com>
2019-04-02 21:25:56 +0300

    make booking model

--- app/Booking.php
+++ app/Booking.php
@@ -4,13 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class RoomImage extends Model
+class Booking extends Model
 {
     use SoftDeletes;
 
-    public $table = 'booking';
+    public $table = '';
     public $primaryKey = 'id';
-    
+    public $fillable = ['location'];
     public $dates = ['created_at', 'updated_at', 'deleted_at'];
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
