a30a7a65a28bb9af80839bb843a0f0cd3ee9752f
Sondos <sondosaljaru@gmail.com>
2019-05-19 11:25:15 +0300

    edit footer style

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,10 @@
-<div class="container-fluid" style="background-color: #828282;">
+<div class="container-fluid" style="background-color: #f5f5f5;">
     <div class="row">
         <div class="col-4"></div>
         <div class="col-4 text-center">
             <img src="{{asset('/control/img/3.png')}}">
 
-            <p style="color:white;">Subscribe to receive exclusive offers and wedding tips.</p>
+            <p style="color:#868383;">Subscribe to receive exclusive offers and wedding tips.</p>
             <div class="input-group mb-3">
                 <input type="text" class="form-control" placeholder="Enter Email Address" aria-label="Enter Email Address" aria-describedby="basic-addon2">
                 <div class="input-group-append">
@@ -16,7 +16,7 @@
     </div>
     <hr>
     <!-- -->
-    <div class="row" style="color: white;">
+    <div class="row" style="color: #868383;">
         <div class="col-3">Terms of use</div>
         <div class="col-3">Privacy policy</div>
         <div class="col-3">
