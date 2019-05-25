d62214c35e82d5c098106f9913ed4c950505289b
aya_habeeb98 <aya.r.h98@gmail.com>
2019-05-25 03:01:57 +0300

    Edin Hall manager view

--- resources/views/hall/managerHall.blade.php
+++ resources/views/hall/managerHall.blade.php
@@ -1,4 +1,3 @@
-
 @extends('master')
 @section('style')
     <style>
@@ -42,7 +41,16 @@
                     <h2 style="font-family:  'Acme', sans-serif; margin-bottom: 2px;">Halls For {{Auth::user()->name}}</h2>
                     <img src="{{asset('control/img/line.png')}}" alt="" class="mx-auto d-block mb-2">
                 </div>
-
+                    @if($msg)
+                    <div class="container text-center my-5">
+                        <div class="alert col-lg-6 mx-auto my-5 py-3 alert-dark">
+                            <h1>{{$msg}}</h1>
+                            <div class="mt-4 mx-auto" style="width: 50%">
+                                <a href="{{route('hall.create')}}" class="btn btn-danger details-btn col-4">Add Hall</a>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
                     @foreach($halls as $hall)
                         <div class="col-lg-4 col-md-6 mb-4">
                             <div class="card">
@@ -51,10 +59,10 @@
                                 </div>
                                 <div class="card-body text-center">
                                     <h5 class="card-title">{{$hall->name}}</h5>
-                                    <p class="card-text">
+                                    <p class="card-text text-center">
                                         <i class="fas fa-map-marker-alt"></i>
                                         <span>{{$hall->address}}</span>
-                                    <div class="text-left room">
+                                    <div class="text-center mb-3">
                                         <i class="fas fa-info-circle"></i>
                                         <span>{{$hall->rooms_num}} Room</span>
                                     </div>
@@ -62,7 +70,10 @@
                                         <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>
                                     </div>
                                     </p>
-                                    <a href="{{route('room.index' , ['hall' => $hall->id])}}" class="btn btn-danger details-btn">More Details .. </a>
+                                    <div class="col-lg-12 mt-3">
+                                <a href="{{route('hall.edit' , ['id' => $hall->id])}}" class="btn btn-danger details-btn col-4">Edit Data</a>
+                                <a href="{{route('hall.destroy' , ['id' => $hall->id])}}" class="btn btn-danger details-btn col-4">Delete</a>
+                            </div>
                                 </div>
                             </div>
                         </div>
